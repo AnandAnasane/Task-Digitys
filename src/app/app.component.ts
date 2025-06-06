3cc93a0f61a692894e6c185844650a5c2d561728
AnandAnasane <aanand08a@gmail.com>
2025-06-07 01:17:33 +0530

    close side bar update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { Component} from '@angular/core';
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
+declare var bootstrap: any;
+
 
 @Component({
   selector: 'app-root',
@@ -23,6 +25,18 @@ export class AppComponent{
   toggleDarkMode() {
     this.darkMode = !this.darkMode;
     localStorage.setItem('darkMode', this.darkMode.toString());
+  }
+
+
+  // To hide sidebar in mobil View
+   closeOffcanvas() {
+    const offcanvasEl = document.getElementById('sidebarOffcanvas');
+    if (offcanvasEl) {
+      const bsOffcanvas = bootstrap.Offcanvas.getInstance(offcanvasEl);
+      if (bsOffcanvas) {
+        bsOffcanvas.hide();
+      }
+    }
   }
 }
 
